5a31b4787b6736a69f197913e3b1cd321fd9774b
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-07-07 12:54:27 +0300

    feat: import firebase methods and push data to firestore

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 document.addEventListener("DOMContentLoaded", initApp);
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import {
+    getDatabase,
+    ref,
+    push,
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+
 function initApp() {
+    const appSettings = {
+        databaseURL:
+            "https://cart-list-798e2-default-rtdb.europe-west1.firebasedatabase.app/",
+    };
+    const app = initializeApp(appSettings);
+    const database = getDatabase(app);
+    const shopingListInDB = ref(database, "shopingList");
+
+    // console.log(database);
+
     const addToListButton = document.querySelector(".addToList__button");
     const addToListInput = document.querySelector(".addToList__input");
 
-    addToListButton.addEventListener("click", () => {
+    function handleInput() {
         const inputValue = addToListInput.value;
-        console.log(inputValue);
-    });
+        push(shopingListInDB, inputValue);
+        addToListInput.value = "";
+    }
+
+    addToListButton.addEventListener("click", handleInput);
 
     addToListInput.addEventListener("keydown", (e) => {
         if (e.key === "Enter") {
-            const inputValue = addToListInput.value;
-            console.log(inputValue);
+            handleInput();
         }
     });
 }
