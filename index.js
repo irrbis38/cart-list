2c036105e4fcf3fb2102f08abcff2ffcda520f84
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-07-07 15:43:42 +0300

    feat: update shopping list by every data base changing

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import {
     getDatabase,
     ref,
     push,
+    onValue,
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
 function initApp() {
@@ -15,8 +16,6 @@ function initApp() {
     const app = initializeApp(appSettings);
     const database = getDatabase(app);
     const shopingListInDB = ref(database, "shopingList");
-
-    // console.log(database);
 
     const shoppingListButton = document.querySelector(".shoppingList__button");
     const shoppingListInput = document.querySelector(".shoppingList__input");
@@ -30,28 +29,44 @@ function initApp() {
 
     function handleInput(elements) {
         const { shoppingListInput } = elements;
-        // push new value to database
-        push(shopingListInDB, shoppingListInput);
 
-        // create new item and append to shopping list
-        appendNewItemToShoppingList(elements);
+        // clear shopping list
+        clearShoppingList();
+
+        // push new value to database
+        push(shopingListInDB, shoppingListInput.value);
 
         // clear input field
         clearInputFiled(elements);
     }
 
-    function appendNewItemToShoppingList(elements) {
-        const { shoppingListInput, shoppingList } = elements;
-        const inputValue = shoppingListInput.value;
+    onValue(shopingListInDB, function (snapshot) {
+        // clear shopping list
+        clearShoppingList();
+
+        //get value from database and convert to array
+        const shoppingListArray = Object.values(snapshot.val());
+
+        // render array of values
+        shoppingListArray.forEach((value) =>
+            appendNewItemToShoppingList(value, shoppingList)
+        );
+    });
+
+    function appendNewItemToShoppingList(value, listEL) {
         const li = document.createElement("LI");
         li.classList.add("shoppingList__item");
-        li.textContent = inputValue;
-        shoppingList.append(li);
+        li.textContent = value;
+        listEL.append(li);
     }
 
     function clearInputFiled(elements) {
         const { shoppingListInput } = elements;
         shoppingListInput.value = "";
+    }
+
+    function clearShoppingList() {
+        shoppingList.innerHTML = "";
     }
 
     shoppingListButton.addEventListener("click", () => {
